c047618c10f7409c624f0a772459d17b1e51f67c
KuntimaKiala <kuntima.kiala@gmail.com>
2023-07-18 12:37:24 +0200

    Ignore

---
+++ CMakeFiles/train.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/train.dir/Matrix.cpp.o"
+  "CMakeFiles/train.dir/Neurons.cpp.o"
+  "CMakeFiles/train.dir/SimpleNeuralNetWork.cpp.o"
+  "CMakeFiles/train.dir/Topology.cpp.o"
+  "CMakeFiles/train.dir/main.cpp.o"
+  "train"
+  "train.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
